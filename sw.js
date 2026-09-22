998c8881746bf815cbc3cb02d48de025200f3465
gorgerich <pachuliya.a@gmail.com>
2026-09-22 23:18:07 +0300

    Sync sw.js from production v3.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='together-v3-3';
+const CACHE='together-v3-4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
